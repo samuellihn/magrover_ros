c96a4526f7b4d02d8aacbd9a4f0c1595e2e2e043
Samuel Lihn <samuellihn@gmail.com>
2023-01-25 15:18:59 -0500

    clean up repo

--- bt_serial_test/target/debug/bt_serial_test.d
+++
@@ -1,1 +0,0 @@
-C:\Users\samli\Documents\magrover_ros\bt_serial_test\target\debug\bt_serial_test.exe: C:\Users\samli\Documents\magrover_ros\bt_serial_test\src\main.rs
--- bt_serial_test/target/debug/deps/bt_serial_test.d
+++
@@ -1,5 +0,0 @@
-C:\Users\samli\Documents\magrover_ros\bt_serial_test\target\debug\deps\bt_serial_test.exe: src\main.rs
-
-C:\Users\samli\Documents\magrover_ros\bt_serial_test\target\debug\deps\bt_serial_test.d: src\main.rs
-
-src\main.rs:
